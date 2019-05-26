44f771c7bc224ad5eaa883be78a3d87b3ae6bd67
JavierR <frjaraur@gmail.com>
2019-05-26 21:40:37 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,51 @@
+FROM php:7.3.5-fpm-alpine3.9
+
+LABEL maintainer="JR"
+
+ENV php_conf /usr/local/etc/php-fpm.conf
+ENV fpm_conf /usr/local/etc/php-fpm.d/www.conf
+ENV php_vars /usr/local/etc/php/conf.d/vars.ini
+
+RUN apk add --update --no-cache bash nginx  supervisor
+
+ADD conf/supervisord.conf /etc/supervisord.conf
+
+RUN rm -Rf /etc/nginx/nginx.conf
+ADD conf/nginx.conf /etc/nginx/nginx.conf
+
+RUN mkdir -p /etc/nginx/sites-available/ && \
+mkdir -p /etc/nginx/sites-enabled/ && \
+rm -Rf /var/www/* && \
+mkdir /var/www/html/ /run/nginx
+ADD conf/nginx-site.conf /etc/nginx/sites-available/default.conf
+RUN ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf
+
+RUN echo "cgi.fix_pathinfo=0" > ${php_vars} &&\
+    echo "upload_max_filesize = 100M"  >> ${php_vars} &&\
+    echo "post_max_size = 100M"  >> ${php_vars} &&\
+    echo "variables_order = \"EGPCS\""  >> ${php_vars} && \
+    echo "memory_limit = 128M"  >> ${php_vars} && \
+    sed -i \
+        -e "s/;catch_workers_output\s*=\s*yes/catch_workers_output = yes/g" \
+        -e "s/pm.max_children = 5/pm.max_children = 4/g" \
+        -e "s/pm.start_servers = 2/pm.start_servers = 3/g" \
+        -e "s/pm.min_spare_servers = 1/pm.min_spare_servers = 2/g" \
+        -e "s/pm.max_spare_servers = 3/pm.max_spare_servers = 4/g" \
+        -e "s/;pm.max_requests = 500/pm.max_requests = 200/g" \
+        -e "s/user = www-data/user = nginx/g" \
+        -e "s/group = www-data/group = nginx/g" \
+        -e "s/;listen.mode = 0660/listen.mode = 0666/g" \
+        -e "s/;listen.owner = www-data/listen.owner = nginx/g" \
+        -e "s/;listen.group = www-data/listen.group = nginx/g" \
+        -e "s/listen = 127.0.0.1:9000/listen = \/var\/run\/php-fpm.sock/g" \
+        -e "s/^;clear_env = no$/clear_env = no/" \
+        ${fpm_conf}
+
+ADD entrypoint.sh /entrypoint.sh
+RUN chmod 755 /entrypoint.sh
+
+ADD src/ /var/www/html/
+
+EXPOSE 80
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,72 @@
+#!/bin/bash -x
+
+# Set custom webroot
+if [ ! -z "$WEBROOT" ]; then
+ sed -i "s#root /var/www/html;#root ${WEBROOT};#g" /etc/nginx/sites-available/default.conf
+else
+ webroot=/var/www/html
+fi
+
+# Enable custom nginx config files if they exist
+if [ -f /var/www/html/conf/nginx/nginx.conf ]; then
+  cp /var/www/html/conf/nginx/nginx.conf /etc/nginx/nginx.conf
+fi
+
+if [ -f /var/www/html/conf/nginx/nginx-site.conf ]; then
+  cp /var/www/html/conf/nginx/nginx-site.conf /etc/nginx/sites-available/default.conf
+fi
+
+if [ -f /var/www/html/conf/nginx/nginx-site-ssl.conf ]; then
+  cp /var/www/html/conf/nginx/nginx-site-ssl.conf /etc/nginx/sites-available/default-ssl.conf
+fi
+
+
+# Prevent config files from being filled to infinity by force of stop and restart the container
+lastlinephpconf="$(grep "." /usr/local/etc/php-fpm.conf | tail -1)"
+if [[ $lastlinephpconf == *"php_flag[display_errors]"* ]]; then
+ sed -i '$ d' /usr/local/etc/php-fpm.conf
+fi
+
+# Display PHP error's or not
+if [[ "$ERRORS" != "1" ]] ; then
+ echo php_flag[display_errors] = off >> /usr/local/etc/php-fpm.d/www.conf
+else
+ echo php_flag[display_errors] = on >> /usr/local/etc/php-fpm.d/www.conf
+fi
+
+# Display Version Details or not
+if [[ "$HIDE_NGINX_HEADERS" == "0" ]] ; then
+ sed -i "s/server_tokens off;/server_tokens on;/g" /etc/nginx/nginx.conf
+else
+ sed -i "s/expose_php = On/expose_php = Off/g" /usr/local/etc/php-fpm.conf
+fi
+
+# Pass real-ip to logs when behind ELB, etc
+if [[ "$REAL_IP_HEADER" == "1" ]] ; then
+ sed -i "s/#real_ip_header X-Forwarded-For;/real_ip_header X-Forwarded-For;/" /etc/nginx/sites-available/default.conf
+ sed -i "s/#set_real_ip_from/set_real_ip_from/" /etc/nginx/sites-available/default.conf
+ if [ ! -z "$REAL_IP_FROM" ]; then
+  sed -i "s#172.16.0.0/12#$REAL_IP_FROM#" /etc/nginx/sites-available/default.conf
+ fi
+fi
+
+# Set the desired timezone
+echo date.timezone=$(cat /etc/TZ) > /usr/local/etc/php/conf.d/timezone.ini
+
+
+if [ ! -z "$PUID" ]; then
+  if [ -z "$PGID" ]; then
+    PGID=${PUID}
+  fi
+  deluser nginx
+  addgroup -g ${PGID} nginx
+  adduser -D -S -h /var/cache/nginx -s /sbin/nologin -G nginx -u ${PUID} nginx
+else
+  if [ -z "$SKIP_CHOWN" ]; then
+    chown -Rf nginx.nginx /var/www/html
+  fi
+fi
+
+# Start supervisord and services
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
+
